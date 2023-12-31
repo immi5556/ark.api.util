fd7b3f6535c98e2b50b249fa742b309ad188f2a5
Immanuel Raj <kimr@solar.dk>
2023-12-31 17:45:28 +0530

    file util changes

---
+++ Ark.Api.util/ark.api.util/FileUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Xml.Linq;
+
+namespace ark.net.util
+{
+    public class FileUtil
+    {
+        public static void Save(string path, string content)
+        {
+            CreateIfNotExistDirectory(System.IO.Path.GetDirectoryName(path));
+            var fil = CreateFileSequence(path);
+            System.IO.File.WriteAllText(fil, content);
+        }
+        public static string CreateIfNotExistDirectory(string dirpath)
+        {
+            if (!Directory.Exists(dirpath))
+                Directory.CreateDirectory(dirpath);
+            return dirpath;
+        }
+        public static string CreateFileSequence(string fullfilepath, int? seq = null)
+        {
+            string i = seq.HasValue ? "_" + seq.ToString() : "";
+            string fpath1 = Path.Combine(Path.GetDirectoryName(fullfilepath), Path.GetFileNameWithoutExtension(fullfilepath) + i.ToString() + Path.GetExtension(fullfilepath));
+            if (!File.Exists(fpath1))
+            {
+                return fpath1;
+            }
+            else
+            {
+                return CreateFileSequence(fullfilepath, ++seq);
+            }
+        }
+    }
+}
