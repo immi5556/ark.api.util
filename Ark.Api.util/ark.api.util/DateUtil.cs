97769bf848c3315a46fdc96391d48a82444703a4
Immanuel Raj <kimr@solar.dk>
2024-02-09 13:36:19 +0530

    util changes.

--- Ark.Api.util/ark.api.util/DateUtil.cs
+++ Ark.Api.util/ark.api.util/DateUtil.cs
@@ -7,6 +7,8 @@ namespace ark.net.util
 {
     public class DateUtil
     {
-        public static Func<string> CurrentTimeStamp = () => $"{DateTime.Now.Year}_{DateTime.Now.Month.ToString().PadLeft(2, '0')}_{DateTime.Now.Day.ToString().PadLeft(2, '0')}";
+        public static Func<string> CurrentDateStamp = () => $"{DateTime.Now.Year}_{DateTime.Now.Month.ToString().PadLeft(2, '0')}_{DateTime.Now.Day.ToString().PadLeft(2, '0')}";
+        public static Func<string> CurrentTimeStamp = () => $"{DateTime.Now.Hour.ToString().PadLeft(2, '0')}_{DateTime.Now.Minute.ToString().PadLeft(2, '0')}_{DateTime.Now.Second.ToString().PadLeft(2, '0')}";
+        public static Func<string> CurrentTimeMsStamp = () => $"{DateTime.Now.Hour.ToString().PadLeft(2, '0')}_{DateTime.Now.Minute.ToString().PadLeft(2, '0')}_{DateTime.Now.Second.ToString().PadLeft(2, '0')}_{DateTime.Now.Millisecond.ToString().PadLeft(2, '0')}";
     }
 }
