dee828a5f3d1f12684dd5698ec45a2cb21e1def9
Immanuel Raj <kimr@solar.dk>
2024-02-16 22:19:28 +0530

    util helper random

---
+++ Ark.Api.util/ark.api.util/ArkHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ark.net.util
+{
+    public class ArkHelper
+    {
+        static Random r = new Random();
+        public static int Random(int max)
+        {
+            return r.Next(max);
+        }
+        public static int Random(int min, int max)
+        {
+            return r.Next(min, max);
+        }
+    }
+}
