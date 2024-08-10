f86c8d464e05c7f1231c2baae49b24acc0e59f22
Immanuel Raj <kimr@solar.dk>
2024-08-10 22:54:47 +0530

    changes. httputil

---
+++ Ark.Api.util/ark.api.util/HttpUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ark.net.util
+{
+    public class HttpUtil
+    {
+        static readonly HttpClient client = new HttpClient();
+        public static async Task Get(string url)
+        {
+            using HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+        }
+        public static async Task<T> Get<T>(string url)
+        {
+            using HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseBody);
+        }
+    }
+}
