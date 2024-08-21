21b0ce9c4b785350c1a39ff2313f0dc918a454e6
Immanuel Raj <kimr@solar.dk>
2024-08-21 10:57:35 +0530

    more changes.

--- Ark.Api.util/ark.api.util/DateUtil.cs
+++ Ark.Api.util/ark.api.util/DateUtil.cs
@@ -13,5 +13,7 @@ namespace ark.net.util
         public static Func<string> CurrentTimeOnlyStamp = () => $"{DateTime.Now.Hour.ToString().PadLeft(2, '0')}_{DateTime.Now.Minute.ToString().PadLeft(2, '0')}_{DateTime.Now.Second.ToString().PadLeft(2, '0')}";
         public static Func<string> CurrentTimeOnlyMsStamp = () => $"{DateTime.Now.Hour.ToString().PadLeft(2, '0')}_{DateTime.Now.Minute.ToString().PadLeft(2, '0')}_{DateTime.Now.Second.ToString().PadLeft(2, '0')}_{DateTime.Now.Millisecond.ToString().PadLeft(6, '0')}"; //millsecond stamp with 6 digits
         public static Func<string> CurrentTimeStamp = () => $"{CurrentDateStamp()}_{CurrentTimeOnlyMsStamp()}";
+        public static Func<double> CurrentEpochTime = () =>  (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+        public static Func<double, DateTime> EpochToDateTime = (epoch) => new DateTime(1970, 1, 1).AddSeconds(epoch);
     }
 }
