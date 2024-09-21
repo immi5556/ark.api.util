fd366d00979161af614186929021ee12bc5a673e
Immanuel Raj <kimr@solar.dk>
2024-09-21 20:48:57 +0530

    more date stamp change

--- Ark.Api.util/ark.api.util/DateUtil.cs
+++ Ark.Api.util/ark.api.util/DateUtil.cs
@@ -7,9 +7,18 @@ namespace ark.net.util
 {
     public class DateUtil
     {
+        //YearStamp
         public static Func<string> CurrentYearStamp = () => $"{DateTime.Now.Year}";
+        public static Func<DateTime?, string> YearStamp = (dt) => $"{(dt.HasValue ? dt.Value.Year : DateTime.Now.Year)}";
+
+        //MonthStamp
         public static Func<string> CurrentMonthStamp = () => $"{DateTime.Now.Year}_{DateTime.Now.Month.ToString().PadLeft(2, '0')}";
+        public static Func<DateTime?, string> MonthStamp = (dt) => $"{(dt.HasValue ? dt.Value.Year : DateTime.Now.Year)}_{(dt.HasValue ? dt.Value.Month : DateTime.Now.Month).ToString().PadLeft(2, '0')}";
+        
+        //DayStamp
         public static Func<string> CurrentDateStamp = () => $"{DateTime.Now.Year}_{DateTime.Now.Month.ToString().PadLeft(2, '0')}_{DateTime.Now.Day.ToString().PadLeft(2, '0')}";
+        public static Func<DateTime?, string> DateStamp = (dt) => $"{(dt.HasValue ? dt.Value.Year : DateTime.Now.Year)}_{(dt.HasValue ? dt.Value.Month : DateTime.Now.Month).ToString().PadLeft(2, '0')}_{(dt.HasValue ? dt.Value.Day : DateTime.Now.Day).ToString().PadLeft(2, '0')}";
+
         public static Func<string> CurrentTimeOnlyStamp = () => $"{DateTime.Now.Hour.ToString().PadLeft(2, '0')}_{DateTime.Now.Minute.ToString().PadLeft(2, '0')}_{DateTime.Now.Second.ToString().PadLeft(2, '0')}";
         public static Func<string> CurrentTimeOnlyMsStamp = () => $"{DateTime.Now.Hour.ToString().PadLeft(2, '0')}_{DateTime.Now.Minute.ToString().PadLeft(2, '0')}_{DateTime.Now.Second.ToString().PadLeft(2, '0')}_{DateTime.Now.Millisecond.ToString().PadLeft(6, '0')}"; //millsecond stamp with 6 digits
         public static Func<string> CurrentTimeStamp = () => $"{CurrentDateStamp()}_{CurrentTimeOnlyMsStamp()}";
